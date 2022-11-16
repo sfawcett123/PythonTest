2f068fdcd58777bde5fb3af49e6c39868a3783eb
Steven Fawcett <64777084+sfawcett123@users.noreply.github.com>
2022-11-16 20:38:51 +0000

    Update setup.py

--- setup.py
+++ setup.py
@@ -1,6 +1,8 @@
+"""Setup Python Library"""
+
 import setuptools
 
-with open("README.md", "r") as fh:
+with open("README.md", "r" , encoding="utf-8" ) as fh:
     long_description = fh.read()
 
 setuptools.setup(
