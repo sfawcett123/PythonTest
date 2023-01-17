a7ce4cde90feca3e0c1f6f02fe2698ed34629ec6
Steve Fawcett <stevenfawcett38@gmail.com>
2023-01-17 10:26:21 +0000

    Do not use non-blocking requests

--- client_test.py
+++ client_test.py
@@ -1,55 +1,54 @@
 """ Module to Test Sim Listener """
 import json
 import socket
+import sys
 import requests
-import socket
-import time
 
-
-host = socket.gethostname() # Get local machine name
-#host = "192.204.3.21"
-
-
-SIM_SERVER_URL = f"https://{host}:27464/api/v1/API/register"
+SIM_SERVER_URL = "https://localhost:27464/api/v1/API/register"
 
 def register():
     """Register with Sim Listener"""
     content = { 'Content-Type': 'application/json' , 'accept': 'text/plain' }
-    data = {  "name": "Python Output Test" }
+    rdata = {  "name": "Python Output Test" }
 
+    print( "Registration data =" , json.dumps(rdata) )
+    try:
+        req = requests.request( "POST" , SIM_SERVER_URL,
+                                         timeout=10,
+                                         headers=content,
+                                         data=json.dumps(rdata),
+                                         verify=False )
+    except requests.exceptions.ConnectionError as e :
+        print( "Server not ready")
+        sys.exit(1)
 
-    print( "DATA =" , json.dumps(data) )
-    req = requests.request( "POST" , SIM_SERVER_URL,
-                                   timeout=10,
-                                   headers=content,
-                                   data=json.dumps(data),
-                                   verify=False )
     print( "RESPONSE = " , req.text )
     return json.loads(req.text)
 
+# Set up some data to send
+json_data = {  "BUTTON POSITION": "ON" }
+data = json.dumps(json_data)
 
+# Currently this is running on same server as tcp server.
+host = socket.gethostname() # Get local machine name
 #j = register()
-#port = int(j['port'])
-port = 9000
-
+#port = int(j['Port'])
+port=9000
 print( f"Connecting on {host}:{port}")
-
-s = socket.socket()         # Create a socket object
 
 # Initialize a TCP client socket using SOCK_STREAM
 tcp_client = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-json_data = {  "BUTTON POSITION": "OFF" }
-data = json.dumps(json_data)
+tcp_client.connect((host, port))
+tcp_client.setblocking( True )
 
-try:
-    # Establish connection to TCP server and exchange data
-    tcp_client.connect((host, port))
-    while True:
-        tcp_client.sendall(data.encode())
+while True:
+    try:
+        tcp_client.sendall(data.encode() )
         received = tcp_client.recv(1024)
-        print ("Bytes Sent:     {}".format(data))
-        print ("Bytes Received: {}".format(received.decode()))    
-finally:
-    tcp_client.close()
-
-               # Close the socket when done
+        print (f"Bytes Received: {received.decode()}" )
+        print (f"Bytes Sent: {data}")
+    except ConnectionResetError as e :
+        print( f"Socket Closed with {e}" )
+        sys.exit(1)
+    except BlockingIOError as e :
+        pass
